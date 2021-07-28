6ed279bf562f726067c9394e6f13ba5fe772293b
WIN-T2O982ERQRV\User <vladislav.drushliak@gmail.com>
2021-07-28 21:12:06 +0300

    Реализовать Add, AddRange, Remove(наброски), RemoveAt, Sort(наброски)

---
+++ Modul3HW1/Modul3HW1/MyList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+public class MyList<T> // : IReadOnlyCollection <T>
+{
+    private int _arraySize = 0;
+    private T[] _array;
+
+    public MyList()
+    {
+        _array = new T[4];
+    }
+
+    public void Add(T value)
+    {
+        if (_arraySize == _array.Length)
+        {
+            Array.Resize<T>(ref _array, _array.Length * 2);
+        }
+
+        _array[_arraySize] = value;
+        _arraySize++;
+    }
+
+    public void AddRange(T[] array)
+    {
+        foreach (var item in array)
+        {
+            Add(item);
+        }
+    }
+
+    public void Remove(T item)
+    {
+        for (var index = 0; index < _array.Length; index++)
+        {
+            if (_array[index].Equals(item))
+            {
+                RemoveAt(index);
+            }
+        }
+    }
+
+    public void RemoveAt(int index)
+    {
+        _array[index] = default(T);
+    }
+
+    public void Sort(IComparer<T> comparer)
+    {
+        Array.Sort(_array, comparer);
+    }
+
+    public T[] GetArray()
+    {
+        return _array;
+    }
+}
--- Modul3HW1/Modul3HW1/Program.cs
+++ Modul3HW1/Modul3HW1/Program.cs
@@ -1,6 +1,18 @@
-﻿public class Program
+﻿using System;
+using System.Collections.Generic;
+public class Program
 {
     public static void Main(string[] args)
     {
+        var array = new MyList<int>();
+
+        array.AddRange(new int[] { 2, 5, 6, 7, 9 });
+
+        array.Sort(new Comparer());
+
+        foreach (var i in array.GetArray())
+        {
+            Console.WriteLine(i);
+        }
     }
 }
